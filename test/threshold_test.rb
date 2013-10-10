9235a2c42c071a6ded87d6ff213738e1cc60e308
Grayson Wright <wright.grayson@gmail.com>
2013-10-10 12:32:26 -0400

    WIP extract function

--- test/threshold_test.rb
+++ test/threshold_test.rb
@@ -21,8 +21,7 @@ describe 'Thresholding' do
 
   private
   def test_threshold_value threshold
-    `ruby threshold.rb -f #{original} -T #{threshold} -o #{ruby_output} 2>&1`
-    `./#{c_executable} -f #{original} -T #{threshold} -o #{c_output} 2>&1`
+    `ruby threshold.rb -f #{original} -T #{threshold} -o #{ruby_output} 2>&1`.must_equal `./#{c_executable} -f #{original} -T #{threshold} -o #{c_output} 2>&1`
     `diff #{c_output} #{ruby_output}`.must_equal ''
   end
 
