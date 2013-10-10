ca2e72819904535fa2b67d37adf413a6be271f1e
Grayson Wright <wright.grayson@gmail.com>
2013-10-10 15:23:12 -0400

    WIP Add width and height methods to Image

--- app/image.rb
+++ app/image.rb
@@ -2,4 +2,12 @@ class Image
   def self.from_pgm filepath
     Image.new
   end
+
+  def height
+    480
+  end
+
+  def width
+    640
+  end
 end
--- test/image_input_test.rb
+++ test/image_input_test.rb
@@ -4,13 +4,30 @@ require 'minitest/autorun'
 describe Image do
   describe '::from_pgm' do
     it 'returns an image instance' do
-      image = Image.from_pgm test_file_name
+      image = Image.from_pgm woman_filepath
       image.must_be_instance_of Image
+    end
+
+    it 'imports the correct witdh and height' do
+      image.height.must_equal expected_woman.height
+      image.width.must_equal expected_woman.width
+    end
+
+    private
+    def image
+      @_image ||= Image.from_pgm woman_filepath
     end
   end
 
   private
-  def test_file_name
+
+  ImageInfo = Struct.new(:filepath, :width, :height)
+
+  def expected_woman
+    @_expected_woman ||= ImageInfo.new 'test_image.pgm', 640, 480
+  end
+
+  def woman_filepath
     'test_image.pgm'
   end
 end
