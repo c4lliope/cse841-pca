8c1c58c5730d1e7451dc5489adbba5230d99665f
Grayson Wright <wright.grayson@gmail.com>
2013-10-10 12:16:15 -0400

    Only test key thresholding values

--- test/threshold_test.rb
+++ test/threshold_test.rb
@@ -1,8 +1,17 @@
 require 'minitest/autorun'
 
 describe 'Thresholding' do
-  `gcc given/example.c`
-  0.upto(255) do |threshold|
+  `gcc given/example.c -o tmp/c_threshold.o`
+
+  def self.test_values
+    (0...255).step(64).to_a + edge_cases
+  end
+
+  def self.edge_cases
+    [0,1,2,9,10,11,254,255]
+  end
+
+  test_values.each do |threshold|
     describe "with a threshold of #{threshold}" do
       it 'generates the same output as the example program' do
         test_threshold_value(threshold)
@@ -12,9 +21,9 @@ describe 'Thresholding' do
 
   private
   def test_threshold_value threshold
-    `ruby example.rb -f #{original} -T #{threshold} -o #{ruby_output(threshold)} 2>&1`
-    `./a.out -f #{original} -T #{threshold} -o #{c_output(threshold)} 2>&1`
-    `diff #{c_output(threshold)} #{ruby_output(threshold)}`.must_equal ''
+    `ruby example.rb -f #{original} -T #{threshold} -o #{ruby_output} 2>&1`
+    `./#{c_executable} -f #{original} -T #{threshold} -o #{c_output} 2>&1`
+    `diff #{c_output} #{ruby_output}`.must_equal ''
   end
 
   def output_dir
@@ -25,11 +34,15 @@ describe 'Thresholding' do
     'test/test_image.pgm'
   end
 
-  def c_output(threshold='')
-    output_dir + "output_#{threshold}_c.pgm"
+  def c_output
+    output_dir + "output_c.pgm"
   end
 
-  def ruby_output(threshold='')
-    output_dir + "output_#{threshold}_rb.pgm"
+  def ruby_output
+    output_dir + "output_rb.pgm"
+  end
+
+  def c_executable
+    output_dir + 'c_threshold.o'
   end
 end
