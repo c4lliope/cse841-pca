2ed58ac0914dce836a4a834b277411ebaf595521
Grayson Wright <wright.grayson@gmail.com>
2013-10-12 19:26:27 -0400

    Hopefully CCIPCA is working

--- app/ccipca.rb
+++ app/ccipca.rb
@@ -11,17 +11,33 @@ class CCIPCA
   def learn vector
     @iteration += 1
     update_mean vector
-    update_eigenvector 1, scatter(vector)
+    if iteration > 1
+      update_eigenvector 1, scatter(vector)
+    end
   end
 
   private
 
   def update_eigenvector index, vector
-    response = vector.dot eigenvector[index].normal
-    @eigenvector[index] = eigenvector[index] * retention_rate +
-      vector * response * learning_rate
-    residual = vector - eigenvector[index].normal * response
-    update_eigenvector index + 1, residual
+    if index >= eigenvectors.count
+      @eigenvectors[index] = vector
+    else
+      response = vector.dot eigenvectors[index].normal
+      retained = eigenvectors[index] * retention_rate
+      learned = vector * response * learning_rate
+      @eigenvectors[index] = retained + learned
+      residual = vector - eigenvectors[index].normal * response
+      update_eigenvector index + 1, residual
+    end
+  end
+
+  def retention_rate
+    amnesia = 0
+    (t - 1.0 - amnesia) / t
+  end
+
+  def learning_rate
+    1.0 - retention_rate
   end
 
   def scatter vector
--- test/ccipca_test.rb
+++ test/ccipca_test.rb
@@ -40,7 +40,7 @@ describe CCIPCA do
       pca = CCIPCA.new
       pca.learn first_vector
       pca.learn second_vector
-      pca.eigenvectors[0].must_equal (second_vector - mean_vector)
+      pca.eigenvectors.last.must_equal (second_vector - mean_vector)
     end
 
     private
@@ -54,6 +54,40 @@ describe CCIPCA do
 
     def mean_vector
       (first_vector + second_vector) / 2
+    end
+  end
+
+  describe 'learning three images' do
+    it 'sets the mean correctly' do
+      learn_vectors
+      pca.mean.must_equal mean_vector
+    end
+
+    it 'sets the eigenvectors' do
+      learn_vectors
+      pca.eigenvectors[1].must_equal Vector.new([1.6428047216990183, 3.9522761100647035, -1.3333333333333333])
+      pca.eigenvectors.last.must_equal Vector.new([-0.156334938729092, -0.10557213329872006, -0.41419548831892833])
+    end
+
+    private
+    def pca
+      @_pca ||= CCIPCA.new
+    end
+
+    def learn_vectors
+      vectors.each { |v| pca.learn v }
+    end
+
+    def vectors
+      [
+        Vector.new([1.0, -2.0, 3.0]),
+        Vector.new([5.0, 8.0, -1.0]),
+        Vector.new([2.0, 1.0, 1.0]),
+      ]
+    end
+
+    def mean_vector
+      vectors.reduce(&:+) / vectors.count
     end
   end
 end
